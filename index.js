618dde8cff68422634097fc0244b9e4e686c7ed9
Taras Mykhailovych <tarquas@gmail.com>
2016-03-23 21:02:42 +0200

    add code

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+require('esfunctional');
+
+let emailVerify = require('email-verify') [promisify]('verify');
+
+spawn(function*() {
+  let emails = process.argv.slice(2);
+
+  yield Promise.all(emails [map]((email) => spawn(function*() {
+    try {
+      let result = yield emailVerify(email);
+      console.log('email:', email, result.success ? '-- exists' : '-- not exists');
+    } catch (err) {
+      switch (err.code) {
+        case 'ENODATA': console.log('email:', email, '-- mail server for a domain is not found'); break;
+        case 'ENOTFOUND': console.log('email:', email, '-- domain is not found'); break;
+        default: console.log('email:', email, '-- unknown error:', err.message); break;
+      }
+    }
+  })));
+
+  console.log('=== All emails checked ===');
+})
+
+[thena](process.exit)
+
+[catcha]((err) => {
+  console.log(
+    err.stack ||
+    err.message ||
+    ('Error: ' + JSON.stringify(err, null, 2))
+  );
+
+  process.exit(1);
+});
